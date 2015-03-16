9b07b4e65995d95ad3e777d90f14ae150fc2cb92
Paulo Diniz <pdiniz_100@yahoo.com>
2015-03-16 09:51:12 -0700

    Secret keys are hidden but database also has pictures and thumbnails

--- app.js
+++ app.js
@@ -25,20 +25,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-var options = [{
-  url: 'https://data.sfgov.org/resource/tkzw-k3nq.json?$limit=50000'
-}, {url: 'https://data.sfgov.org/resource/tkzw-k3nq.json?$limit=50000&$offset=50000'}];
 var conString = "postgres://postgres:1234@localhost/postgres";
-function callback(error, response, body) {
+
+
+var treeInsert = function(error, response, body) {
+  console.log("inside request");
   console.log(error, "error");
   var trees = JSON.parse(body);
-  console.log(body);
+  //console.log(body);
   //console.log(trees[0].qspecies);
   pg.connect(conString, function(err, client, done) {
     console.log(err);
     var locationQuery = "INSERT INTO location (xcoord, ycoord , latitude, longitude) select $1, $2, $3, $4 WHERE NOT EXISTS (SELECT xcoord FROM location WHERE xcoord = $1 and ycoord = $2);";
     var treeQuery = "INSERT INTO tree (name, qspeciesid, siteorder, qsiteinfo, qcaretaker, plantdate, dbh, plotsize, permitnotes, treeid, locationid) SELECT 'tree', (select distinct qspeciesid from qspecies where qspecies = $1 limit 1), $2, $3, $4, $5, $6, $7, $8, $9, (select distinct locationid from location where xcoord = $10 limit 1) WHERE NOT EXISTS (SELECT treeid FROM tree WHERE treeid = $9);";
-    var qspeciesQuery = "INSERT INTO qspecies (qspecies,  picture) SELECT $1, $2 WHERE NOT EXISTS (SELECT qspecies FROM qspecies WHERE qspecies = $1);";
+    var qspeciesQuery = "INSERT INTO qspecies (qspecies) SELECT $1 WHERE NOT EXISTS (SELECT qspecies FROM qspecies WHERE qspecies = $1);";
     async.map(trees, function(tree, cb) {
       var longitude,
         latitude,
@@ -62,7 +62,7 @@ function callback(error, response, body) {
       done();
     });
     async.map(trees, function(tree, cb) {
-      client.query(qspeciesQuery, [tree.qspecies, 'nameless']);
+      client.query(qspeciesQuery, [tree.qspecies]);
     }, function(error, results) {
       console.log("Finished qspecies inserts!", error, results);
       done();
@@ -84,14 +84,86 @@ function callback(error, response, body) {
       done();
     });
   });
-
-}
-var testFunction = function(options){
-  request(options, callback);
 };
-async.map(options, testFunction, function(err, results){
-  console.log(err);
-  console.log(results);
-});
+
+var imageInsert = function() {
+  pg.connect(conString, function(err, client, done) {
+    console.log(err);
+    var selectQuery = "select * from qspecies";
+    client.query(selectQuery, function(err, results) {
+      console.log(err, '   inside select for image');
+      var queryResults = results.rows;
+      var insertFunction = function(qspecies) {
+        //console.log(qspecies);
+        //console.log(qspecies.qspecies);
+        //console.log(qspecies.qspeciesid);
+        var qspeciesid = (qspecies.qspeciesid);
+        var qspeciesname = (qspecies.qspecies);
+        var qspeciesname = qspeciesname;
+        var uri = "https://api.datamarket.azure.com/Bing/Search/v1/Image?Query='" + encodeURIComponent(qspeciesname) + "'&$top=1&$format=json";
+        //console.log(uri);
+        var options = {
+          uri: uri,
+          auth: {
+            user: 'secret',
+            pass: 'secret'
+          }
+        };
+
+        var selectQuery = "SELECT * FROM image WHERE qspeciesid = $1;";
+        client.query(selectQuery, [qspeciesid], function(err, results) {
+          if (results.rows === undefined || results.rows[0] === undefined){
+            console.log('database results', results);
+            console.log('database rows', results.rows);
+            request(options, function(err, response, results, body) {
+              console.log(err, ' error inside picture request');
+              var results = JSON.parse(results);
+              console.log(results);
+              console.log(results['d']);
+              console.log(results.d.results[0]);
+              var mediaurl = results.d.results[0] ? results.d.results[0].MediaUrl : "unknown";
+              var mediaw = results.d.results[0] ? results.d.results[0].Width : 9999;
+              var mediah = results.d.results[0] ? results.d.results[0].Height : 9999;
+              var mediat = results.d.results[0] ? results.d.results[0].ContentType : "unknown";
+              var thumbnailurl = results.d.results[0] ? results.d.results[0].Thumbnail.MediaUrl : "unknown";
+              var thumbnailt = results.d.results[0] ? results.d.results[0].Thumbnail.ContentType : "unknown";
+              var thumbnailw = results.d.results[0] ? results.d.results[0].Thumbnail.Width : 9999;
+              var thumbnailh = results.d.results[0] ? results.d.results[0].Thumbnail.Height : 9999;
+              var insertQuery = "INSERT INTO image (imageurl, imagewidth, imageheight, imagetype, qspeciesid) values ($1, $2, $3, $4, $5);";
+              client.query(insertQuery, [mediaurl, mediaw, mediah, mediat, qspeciesid], function(err, results) {
+                console.log(err, " error inside insert query");
+                console.log(results);
+              });
+              var insertQuery2 = "INSERT INTO thumbnail (contenttype, url, width, height, qspeciesid) values ($1, $2, $3, $4, $5);";
+              client.query(insertQuery2, [thumbnailt, thumbnailurl, thumbnailw, thumbnailh, qspeciesid], function(err, results) {
+                console.log(err, " error inside insert query");
+                console.log(results);
+              });
+            });
+          }
+        });
+      };
+        async.map(queryResults, insertFunction, function(err, results) {
+          console.log(results);
+          console.log(err);
+        });
+    });
+  });
+};
+
+var options = {
+  url: 'https://data.sfgov.org/resource/tkzw-k3nq.json?$limit=50000'
+};
+var options1 = {
+  url: 'https://data.sfgov.org/resource/tkzw-k3nq.json?$limit=50000&$offset=50000'
+};
+
+async.series([
+  function(callback){request(options, treeInsert); callback(null, 'ok')},
+  function(callback){request(options1, treeInsert); callback(null, 'ok')},
+  function(callback){imageInsert(); callback(null, 'ok')}
+]);
+
+
 
 module.exports = app;
